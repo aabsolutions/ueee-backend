1f582038d0f971969e4bd9244a443be9b392bd35
aabsolutions <angelbs.1985@gmail.com>
2024-05-19 21:20:58 -0500

    feat aditional fields of Estudiante model
    Added to Estudiante model:
        direccion
        email
        celular
        representante_cedula
        representante_nombre_completo
        representante_email
        representante_celular
        padre_cedula
        padre_nombre_completo
        padre_celular
        madre_cedula
        madre_nombre_completo
        madre_celular
        discapacidad
        discapacidad_detalle
        enfermedad_catastrofica
        enfermedad_catastrofica_detalle
        alergia
        alergia_detalle
        embarazo

--- models/estudiante.js
+++ models/estudiante.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const { boolean } = require('webidl-conversions');
 
 const EstudianteSchema = Schema({
 
@@ -22,6 +23,70 @@ const EstudianteSchema = Schema({
     sexo: {
         type: Number,
         required: true
+    },
+    direccion: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    celular: {
+        type: String
+    },
+    representante_cedula: {
+        type: String
+    },
+    representante_nombre_completo: {
+        type: String
+    },
+    representante_email: {
+        type: String
+    },
+    representante_celular: {
+        type: String
+    },
+    padre_cedula: {
+        type: String
+    },
+    padre_nombre_completo: {
+        type: String
+    },
+    padre_celular: {
+        type: String
+    },
+    madre_cedula: {
+        type: String
+    },
+    madre_nombre_completo: {
+        type: String
+    },
+    madre_celular: {
+        type: String
+    },
+    discapacidad: {
+        type: Boolean,
+        default: false
+    },
+    discapacidad_detalle: {
+        type: String
+    },
+    enfermedad_catastrofica: {
+        type: Boolean,
+        default: false
+    },
+    enfermedad_catastrofica_detalle: {
+        type: String
+    },
+    alergia: {
+        type: Boolean,
+        default: false
+    },
+    alergia_detalle: {
+        type: String
+    },
+    embarazo: {
+        type: Boolean,
+        default: false
     },
     img: {
         type: String
